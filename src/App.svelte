59db80e33f34ce009f33af0e1cb9bf693a33db7b
mia-z <iperish.x@gmail.com>
2022-11-28 18:16:54 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Footer from "./components/Footer.svelte";
+	import Header from "./components/Header.svelte";
+	import { DarkMode } from "./lib/DarkMode.store";
+</script>
+
+<main id={"main"} class={`${$DarkMode ? "dark" : ""} preload`}>
+	<div class={"bg-[whitesmoke] transition-colors dark:bg-slate-800 dark:text-white min-h-screen flex flex-col"}>
+		<Header />
+		<div class={"sm:container mx-auto flex flex-col"}>
+			<!-- BODY HERE -->
+		</div>
+		<Footer />
+	</div>
+</main>
